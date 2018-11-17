926edcd2926e0a8d91a81d529b6118a85f640d81
Skye Viau <skye.viau@gmail.com>
2018-11-16 23:08:40 -0500

    Working bot

--- bot.py
+++ bot.py
@@ -1,1 +1,35 @@
-print "test"
+import discord
+import asyncio
+import json
+import os
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+configJSON = open(os.path.join(__location__, "config.json"), "r")
+
+config = json.load(configJSON)
+botToken = config["token"]
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run(botToken)
