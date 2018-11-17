3b9e052d13c5d46c76ef040b3c972d74e8de6dc0
AsornXD <flamescorpions10@gmail.com>
2018-11-17 13:11:13 -0500

    bot thing

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@ import discord
 import asyncio
 import json
 import os
+import random
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 configJSON = open(os.path.join(__location__, "config.json"), "r")
 
 config = json.load(configJSON)
 botToken = config["token"]
-prefix = "!"
+prefix = "hey asta! "
 
 client = discord.Client()
 
@@ -21,13 +22,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    message.content = message.content.lower()
     if message.content.startswith(prefix+'test'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
             if log.author == message.author:
                 counter += 1
-
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
     elif message.content.startswith(prefix+'sleep'):
         await asyncio.sleep(5)
@@ -35,4 +36,20 @@ async def on_message(message):
     elif message.content.startswith(prefix+'osu'):
         await client.send_message(message.channel, 'https://lemmmy.pw/osusig/sig.php?colour=pink&uname='+message.content.strip(prefix+'osu '))
         print(message.author.name+" just ran !osu with the username being searched "+message.content.strip(prefix+'osu '))
-client.run(botToken)
+    elif message.content.startswith(prefix+'eightball') or message.content.startswith(prefix+'8ball') or message.content.startswith(prefix+'8-ball') or message.content.startswith(prefix+'eight-ball'):
+        num = random.randint(0,6)
+        if num == 0:
+            await client.send_message(message.channel, 'Certainly!')
+        elif num == 1:
+            await client.send_message(message.channel, 'Most likely')
+        elif num == 2:
+            await client.send_message(message.channel, "I'm not sure :/")
+        elif num == 3:
+            await client.send_message(message.channel, 'Unlikely')
+        elif num == 4:
+            await client.send_message(message.channel, 'Yes!' )
+        elif num == 5:
+            await client.send_message(message.channel, 'No!')
+        elif num == 6:
+            await client.send_message(message.channel, 'Are you crazy?!')
+client.run(botToken)    
