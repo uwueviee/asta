dcac1d53696cd1247dd4d0ad9744af9b4b501c65
Skye Viau <skye.viau@gmail.com>
2018-11-17 00:52:50 -0500

    Add windows for terminal

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
+    if message.content.startswith(prefix+'test'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
@@ -29,9 +29,9 @@ async def on_message(message):
                 counter += 1
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
+    elif message.content.startswith(prefix+'sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
-    elif message.content.startswith('!osu'):
-        await client.send_message(message.channel, 'https://lemmmy.pw/osusig/sig.php?colour=pink&uname='+message.content.strip('!osu '))
+    elif message.content.startswith(prefix+'osu'):
+        await client.send_message(message.channel, 'https://lemmmy.pw/osusig/sig.php?colour=pink&uname='+message.content.strip(prefix+'osu '))
 client.run(botToken)
