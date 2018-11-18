80780bb10c3e8d4dbd98b812ae89931a3747a994
Skye Viau <skye.viau@gmail.com>
2018-11-18 12:03:12 -0500

    Change test to ping and remove sleep

--- bot.py
+++ bot.py
@@ -27,16 +27,13 @@ async def on_ready():
 @client.event
 async def on_message(message):
     message.content = message.content.lower()
-    if message.content.startswith(prefix+'test'):
+    if message.content.startswith(prefix+'ping'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
             if log.author == message.author:
                 counter += 1
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith(prefix+'sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
     elif message.content.startswith(prefix+'osu'):
         newmessage = message.content.split(' ')
         await client.send_message(message.channel, 'https://lemmmy.pw/osusig/sig.php?colour=pink&uname='+newmessage[3])
